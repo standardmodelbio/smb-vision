15eeb7058e6b817ceacc8e55913f0c11e2e31c7c
Zekai Chen <zach@Zekais-MacBook-Air.local>
2024-10-29 20:12:00 -0700

    increase batch size

--- run_mim.sh
+++ run_mim.sh
@@ -10,7 +10,7 @@ python run_mim.py \
     --max_grad_norm 1.0 \
     --warmup_ratio 0.02 \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --do_train true \
