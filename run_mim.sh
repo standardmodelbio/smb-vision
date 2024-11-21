500f65f46eb2864f173a2bb676ff1fd8dd912d13
chenz53 <zach@standardmodel.bio>
2024-11-20 22:34:37 -0800

    tests

--- run_mim.sh
+++ run_mim.sh
@@ -2,6 +2,8 @@
 export WANDB_PROJECT=smb-vision
 export WANDB_LOG_MODEL=checkpoint
 
+python scripts/download_from_s3.py
+
 python src/run_mim.py \
     --json_path ./smb-vision-large-train-mim.json \
     --cache_dir ../cache/ \
@@ -9,7 +11,7 @@ python src/run_mim.py \
     --lr_scheduler_type cosine \
     --max_grad_norm 1.0 \
     --warmup_steps 200 \
-    --max_steps 2400 \
+    --num_train_epochs 30 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
@@ -19,7 +21,7 @@ python src/run_mim.py \
     --output_dir ./saves/smb-vision-large-1120 \
     --eval_strategy "no" \
     --eval_steps 500 \
-    --save_steps 500 \
+    --save_steps 1000 \
     --gradient_checkpointing true \
     --bf16 true \
     --logging_steps 1 \
