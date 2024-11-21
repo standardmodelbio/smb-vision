8cecd5a0896f3e11d4549c0c74787f6e860da93f
chenz53 <zach@standardmodel.bio>
2024-11-20 17:36:45 -0800

    tests

--- src/dataloader/build_train_file.py
+++ src/dataloader/build_train_file.py
@@ -41,7 +41,7 @@ def get_transforms(img_size=384, depth=320, mask_patch_size=32, patch_size=16, m
                 b_max=1.0,
                 clip=True,
             ),
-            CropForegroundd(keys=["image"], source_key="image", allow_smaller=False),
+            # CropForegroundd(keys=["image"], source_key="image", allow_smaller=False),
             RandSpatialCropSamplesd(
                 keys=["image"],
                 roi_size=(img_size, img_size, depth),
