df6db4e03fa7ff5deaf83aebc78a214c0b40bd64
chenz53 <zach@standardmodel.bio>
2024-11-20 16:55:22 -0800

    tests

--- src/dataloader/mim.py
+++ src/dataloader/mim.py
@@ -164,15 +164,15 @@ class MIMDataset:
                     clip=True,
                 ),
                 CropForegroundd(keys=["image"], source_key="image"),
+                SpatialPadd(
+                    keys=["image"],
+                    spatial_size=(self.img_size, self.img_size, self.depth),
+                ),
                 RandSpatialCropSamplesd(
                     keys=["image"],
                     roi_size=(self.img_size, self.img_size, self.depth),
                     random_size=False,
                     num_samples=1,
-                ),
-                SpatialPadd(
-                    keys=["image"],
-                    spatial_size=(self.img_size, self.img_size, self.depth),
                 ),
                 # RandScaleIntensityd(keys="image", factors=0.1, prob=0.5),
                 # RandShiftIntensityd(keys="image", offsets=0.1, prob=0.5),
