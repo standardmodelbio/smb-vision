9067edbcfa4aecfe9c0bc145cb52b8e9290c32a4
chenz53 <zach@standardmodel.bio>
2024-11-23 16:29:14 -0800

    updates

--- run_mim.sh
+++ run_mim.sh
@@ -10,21 +10,21 @@ python src/run_mim.py \
     --learning_rate 3e-4 \
     --lr_scheduler_type cosine \
     --max_grad_norm 1.0 \
-    --warmup_steps 200 \
+    --warmup_ratio 0.01 \
     --num_train_epochs 10 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --do_train true \
     --do_eval true \
     --overwrite_output_dir true \
-    --output_dir ./saves/smb-vision-large-1120 \
+    --output_dir ./saves/smb-vision-large-1123 \
     --eval_strategy "no" \
     --eval_steps 500 \
     --save_steps 1000 \
     --bf16 true \
     --deepspeed ds_configs/ds_z2_config.json \
-    --gradient_checkpointing false \
+    --gradient_checkpointing true \
     --logging_steps 1 \
     --report_to wandb \
-    --run_name smb-vision-large-1120
+    --run_name smb-vision-large-1123
