367f8c4cecd8fdff33cc7a4858361098f22f9ab7
chenz53 <zach@standardmodel.bio>
2024-11-28 13:54:08 -0800

    updates

--- run_mim.sh
+++ run_mim.sh
@@ -31,7 +31,6 @@ python src/run_mim.py \
     --save_steps 500 \
     --bf16 true \
     --gradient_checkpointing true \
-    --deepspeed ds_configs/ds_z2_config.json \
     --resume_from_checkpoint saves/smb-vision-large-1125/checkpoint-7900 \
     --logging_steps 1 \
     --report_to wandb \
