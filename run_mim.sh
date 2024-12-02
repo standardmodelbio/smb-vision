c08297ef0ead622b0b93eb1d8e4547bc84bacca4
chenz53 <zach@standardmodel.bio>
2024-12-02 13:42:28 -0800

    updates

--- run_mim.sh
+++ run_mim.sh
@@ -31,7 +31,7 @@ python src/run_mim.py \
     --save_steps 500 \
     --bf16 true \
     --gradient_checkpointing true \
-    --resume_from_checkpoint saves/smb-vision-large-1125/checkpoint-15800 \
+    --resume_from_checkpoint saves/smb-vision-large-1125/checkpoint-17200 \
     --logging_steps 1 \
     --report_to wandb \
     --run_name smb-vision-large-1125
