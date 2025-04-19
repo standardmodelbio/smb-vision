b9dca7da3be35db707510175f21dd28c61631255
chenz53 <zach@standardmodel.bio>
2025-04-19 18:18:33 -0400

    updates

---
+++ scripts/inference/run_inspect.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+HOME=/workspace/inspect
+
+python scripts/inference/inspect.py \
+    --impressions_path $HOME/Final_Impressions.csv \
+    --image_dir $HOME/CTPA/ \
+    --saved_json_path /tmp/asset-inspect.json \
+    --cache_dir $HOME/cache \
+    --save_path s3://smb-data-us-east-2/embeddings/inspect
