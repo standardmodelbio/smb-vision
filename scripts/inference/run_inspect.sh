b99d5fd9585fc46eed65d80ba26bbb4835b885ee
chenz53 <zach@standardmodel.bio>
2025-04-19 22:31:43 -0400

    updates for multithreads inference

--- scripts/inference/run_inspect.sh
+++ scripts/inference/run_inspect.sh
@@ -3,9 +3,8 @@
 HOME=/workspace/inspect
 
 python scripts/inference/run_inspect.py \
-    --impressions_path $HOME/Final_Impressions.csv \
     --image_dir $HOME/CTPA/ \
     --saved_json_path $HOME/asset-inspect.json \
     --cache_dir $HOME/cache \
-    --save_path s3://smb-data-us-east-2/embeddings/inspect \
+    --save_path s3://smb-data-us-east-2/embeddings/dataset=inspect \
     --bf16
