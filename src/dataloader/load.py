f59c281f4197efc16e563793e5bcb629555795e4
chenz53 <zach@standardmodel.bio>
2025-04-21 23:26:52 -0400

    updates

--- src/dataloader/load.py
+++ src/dataloader/load.py
@@ -23,7 +23,7 @@ class CTDataset:
         self.cache_rate = args.cache_rate
         self.cache_dir = args.cache_dir
         self.dist = args.dist
-        self.model_class = args.model_class
+        self.model_class = args.encoder
 
     def val_transforms(
         self,
