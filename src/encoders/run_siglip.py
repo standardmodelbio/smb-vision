d6868ed108d4a354baf7540a9b7f0cef332f53fd
chenz53 <zach@standardmodel.bio>
2025-05-06 00:22:46 -0400

    updates

--- src/encoders/run_siglip.py
+++ src/encoders/run_siglip.py
@@ -74,7 +74,7 @@ class SiglipEncoder(BaseEncoder):
             images = images.to(self.device)
             with torch.inference_mode():
                 outputs = model(images, output_hidden_states=True)
-                print(outputs)
+                print(outputs.hidden_states.shape)
                 outputs = outputs.hidden_states[-2]
                 embeddings = outputs[:, 1:]
                 # embeddings = outputs / outputs.norm(dim=-1, keepdim=True)
