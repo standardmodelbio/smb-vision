0cf71db8df2879e71145712fcb5a9bfeddd11616
chenz53 <zach@standardmodel.bio>
2025-05-16 03:08:16 -0400

    updates

--- src/run_mim.py
+++ src/run_mim.py
@@ -196,8 +196,8 @@ class ModelArguments:
 
 def collate_fn(examples):
     for example in examples:
-        print(example["image"].shape)
-        print(example["mask"].shape)
+        print(example.keys())
+        # print(example["mask"].shape)
     pixel_values = torch.stack([example["image"] for example in examples])
     mask = torch.stack([example["mask"] for example in examples])
     return {"pixel_values": pixel_values, "bool_masked_pos": mask}
