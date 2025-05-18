f417f3419a362749d6ca5714694669a092ca687b
chenz53 <zach@standardmodel.bio>
2025-05-18 01:30:15 -0400

    updates

--- scripts/training/run_mim.sh
+++ scripts/training/run_mim.sh
@@ -12,27 +12,27 @@ export WANDB_PROJECT=smb-vision
 
 # train
 accelerate launch src/run_mim.py \
-    --json_path /home/user/smb_vision_train_dataset.json \
-    --cache_dir /home/user/cache/ \
+    --json_path /workspace/smb_vision_dataset.json \
+    --cache_dir /workspace/cache/ \
     --model_name_or_path standardmodelbio/smb-vision-base \
     --learning_rate 5e-5 \
     --lr_scheduler_type cosine \
     --max_grad_norm 1.0 \
     --warmup_ratio 0.01 \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 2 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 8 \
     --do_train true \
     --do_eval true \
     --overwrite_output_dir true \
     --remove_unused_columns false \
-    --output_dir /home/user/saves/smb-vision-base-05152025 \
+    --output_dir /workspace/saves/smb-vision-base-05152025 \
     --eval_strategy "no" \
     --eval_steps 500 \
     --save_steps 500 \
     --bf16 true \
     --gradient_checkpointing true \
     --logging_steps 1 \
-    --report_to none \
+    --report_to wandb \
     --run_name smb-vision-base-05152025
