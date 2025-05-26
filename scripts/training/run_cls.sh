0e46c5431e2ce149824a0f55c31bcb300d6d82bc
chenz53 <zach@standardmodel.bio>
2025-05-26 16:52:11 -0400

    updates

--- scripts/training/run_cls.sh
+++ scripts/training/run_cls.sh
@@ -3,29 +3,28 @@ export WANDB_PROJECT=smb-vision-cls
 
 # Paths and model configuration
 # TODO: change these paths to your own
-OUTPUT_DIR=./saves/smb-vision-cls-05202025
-DATA_PATH=./dummy_data/dataset.parquet
-DATA_CACHE_PATH=./cache/
-MODEL_NAME=standardmodelbio/smb-vision-base
-RUN_NAME=smb-vision-cls-05202025
+OUTPUT_DIR=/workspace/saves/smb-vision-cls-05202025
+DATA_PATH=/workspace/data/mdanderson_dataset.parquet
+DATA_CACHE_PATH=/workspace/cache/
+MODEL_NAME=standardmodelbio/smb-vision-ct-base-0519
+RUN_NAME=smb-vision-survival-mdacc
 
 # Model parameters
 # TODO: change these parameters to your own
-NUM_LABELS=2
+NUM_LABELS=1
 LEARNING_RATE=1e-5
 VISION_LR=1e-5
 MERGER_LR=5e-5
 WEIGHT_DECAY=0.1
 MAX_GRAD_NORM=1.0
 WARMUP_RATIO=0.01
-NUM_EPOCHS=1
-LABEL_COLUMNS="histology_adc histology_squ histology_smc"
+NUM_EPOCHS=10
 
 # Batch size and device configuration
 # TODO: change these parameters to your own
-GLOBAL_BATCH_SIZE=256
+GLOBAL_BATCH_SIZE=16
 BATCH_PER_DEVICE=16
-NUM_DEVICES=8
+NUM_DEVICES=1
 GRAD_ACCUM_STEPS=$((GLOBAL_BATCH_SIZE / (BATCH_PER_DEVICE * NUM_DEVICES)))
 
 # train
@@ -34,7 +33,7 @@ accelerate launch src/run_classification.py \
     --val_data_path $DATA_PATH \
     --cache_dir $DATA_CACHE_PATH \
     --model_name_or_path $MODEL_NAME \
-    --task_type multilabel_classification \
+    --task_type survival \
     --num_labels $NUM_LABELS \
     --lr_scheduler_type cosine \
     --learning_rate $LEARNING_RATE \
@@ -50,16 +49,15 @@ accelerate launch src/run_classification.py \
     --do_train true \
     --do_eval true \
     --overwrite_output_dir true \
-    --label_columns $LABEL_COLUMNS \
     --remove_unused_columns false \
     --output_dir $OUTPUT_DIR \
     --eval_strategy "steps" \
-    --eval_steps 500 \
+    --eval_steps 2 \
     --save_strategy "steps" \
-    --save_steps 500 \
+    --save_steps 10 \
     --save_total_limit 10 \
     --load_best_model_at_end true \
-    --metric_for_best_model "accuracy" \
+    --metric_for_best_model "c_index" \
     --greater_is_better true \
     --bf16 true \
     --gradient_checkpointing true \
