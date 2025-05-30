31eb047e3e015757dbfcd8456bcf20a2a928f37f
chenz53 <zach@standardmodel.bio>
2025-05-30 19:08:06 -0400

    updates

--- src/dataloader/transforms.py
+++ src/dataloader/transforms.py
@@ -131,6 +131,22 @@ ct_transforms = {
             PermuteImage(),
         ],
     ),
+    "dinov2": Compose(
+        [
+            LoadImaged(keys=["image"]),
+            EnsureChannelFirstd(keys=["image"]),
+            Orientationd(keys=["image"], axcodes="RAS"),
+            Spacingd(keys=["image"], pixdim=(1.5, 1.5, 3.0), mode=("bilinear")),
+            ScaleIntensityRanged(keys=["image"], a_min=-1000, a_max=1000, b_min=0.0, b_max=1.0, clip=True),
+            SpatialPadd(keys=["image"], spatial_size=[224, 224, 160]),
+            CenterSpatialCropd(
+                roi_size=[224, 224, 160],
+                keys=["image"],
+            ),
+            # ToTensord(keys=["image"]),
+            # PermuteImage(),
+        ],
+    ),
     "merlin": Compose(
         [
             LoadImaged(keys=["image"]),
