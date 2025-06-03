6a23511c52c0b93d6e8884fcd32e4d04cf7a5fa6
chenz53 <zach@standardmodel.bio>
2025-06-03 19:08:46 -0400

    updates

--- scripts/training/run_cls.sh
+++ scripts/training/run_cls.sh
@@ -12,7 +12,7 @@ RUN_NAME=smb-vision-base-survival-mdacc
 # Model parameters
 # TODO: change these parameters to your own
 LABEL_COLUMNS="one_year_survival"
-ADDITIONAL_FEATURE_COLUMNS=None
+ADDITIONAL_FEATURE_COLUMNS=""
 NUM_LABELS=2
 LEARNING_RATE=1e-5
 VISION_LR=1e-5
